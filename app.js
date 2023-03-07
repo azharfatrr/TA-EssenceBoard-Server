e933eb90ed6199279e48af2ab8b9fa6b6c73f6e8
azharfatrr <m.azhar.faturahman@gmail.com>
2023-03-07 15:25:44 +0700

    Change put method to include create new essence_kernel

--- app.js
+++ app.js
@@ -8,16 +8,16 @@ var bodyParser = require('koa-bodyparser');
 const cors = require('@koa/cors');
 const db = require('./res/mongo');
 const ObjectID = require("mongodb").ObjectID;
-require('koa-qs')(app, 'strict')
+require('koa-qs')(app, 'strict');
 const fs = require('fs');
 
 app.use(bodyParser({
-    detectJSON: function (ctx) {
-        return /\.json$/i.test(ctx.path);
-    },
-    extendTypes: {
-        json: ['application/x-javascript'] // will parse application/x-javascript type body as a JSON string
-    },
+  detectJSON: function (ctx) {
+    return /\.json$/i.test(ctx.path);
+  },
+  extendTypes: {
+    json: ['application/x-javascript'] // will parse application/x-javascript type body as a JSON string
+  },
 
 }));
 app.use(cors());
@@ -25,147 +25,153 @@ app.use(cors());
 const index = new router();
 
 index.get('/', function (ctx, next) {
-    ctx.status = 200;
-    ctx.body = "Essence Editor Server is Online";
+  ctx.status = 200;
+  ctx.body = "Essence Editor Server is Online";
 });
 
 // List all people
 index.get("/method", async (ctx) => {
-    await MongoClient.connect(MONGO_URL, { useNewUrlParser: true })
-        .then(async (connection) => {
-            await connection.db("essence").collection('method').find({}).toArray().then((result) => {
-                console.log("Database connection established")
-                ctx.body = JSON.stringify(result)
-            }).catch((err) => console.error(err));
+  await MongoClient.connect(MONGO_URL, { useNewUrlParser: true })
+    .then(async (connection) => {
+      await connection.db("essence").collection('method').find({}).toArray().then((result) => {
+        console.log("Database connection established");
+        ctx.body = JSON.stringify(result);
+      }).catch((err) => console.error(err));
 
 
-        })
-        .catch((err) => console.error(err))
+    })
+    .catch((err) => console.error(err));
 });
 
 index.get("/method/:id", async (ctx) => {
-    await MongoClient.connect(MONGO_URL, { useNewUrlParser: true })
-        .then(async (connection) => {
-            await connection.db("essence")
-                .collection('method')
-                .findOne({"_id": ObjectID(ctx.params.id)})
-                .then((result) => {
-                console.log("Database connection established")
-                ctx.body = JSON.stringify(result)
-            }).catch((err) => console.error(err));
+  await MongoClient.connect(MONGO_URL, { useNewUrlParser: true })
+    .then(async (connection) => {
+      await connection.db("essence")
+        .collection('method')
+        .findOne({ "_id": ObjectID(ctx.params.id) })
+        .then((result) => {
+          console.log("Database connection established");
+          ctx.body = JSON.stringify(result);
+        }).catch((err) => console.error(err));
 
 
-        })
-        .catch((err) => console.error(err))
+    })
+    .catch((err) => console.error(err));
 });
 
 index.put("/method/:id", async (ctx) => {
+  var newValues = { $set: ctx.request.body };
+  let connection = await MongoClient.connect(MONGO_URL, { useNewUrlParser: true });
 
-    var newvalues = { $set: ctx.request.body };
-    await MongoClient.connect(MONGO_URL, { useNewUrlParser: true })
-        .then(async (connection) => {
-            await connection.db("essence").collection('method')
-                .updateOne({"_id": ObjectID(ctx.params.id)},newvalues,{upsert : true}).then((result) => {
-                console.log("Database connection established")
-                ctx.body = JSON.stringify(result)
-            }).catch((err) => console.error(err));
+  if (ObjectID.isValid(ctx.params.id)) {
+    await connection.db("essence").collection('method')
+      .updateOne({ "_id": ObjectID(ctx.params.id) }, newValues, { upsert: true })
+      .then((result) => {
+        ctx.body = JSON.stringify(result);
+      })
+      .catch((err) => console.error(err));
+    return;
+  }
 
-
-        })
-        .catch((err) => console.error(err))
+  await connection.db("essence").collection('method')
+    .updateOne({ "nameId": ctx.params.id }, newValues, { upsert: true })
+    .then((result) => {
+      ctx.body = JSON.stringify(result);
+    })
+    .catch((err) => console.error(err));
 });
 
 index.post("/method", async (ctx) => {
-    console.log(ctx.request.body)
-    await MongoClient.connect(MONGO_URL, { useNewUrlParser: true })
-        .then(async (connection) => {
-            await connection.db("essence")
-                .collection('method')
-                .insertOne(ctx.request.body)
-                .then((result) => {
-                console.log("Created Data Method")
+  console.log(ctx.request.body);
+  await MongoClient.connect(MONGO_URL, { useNewUrlParser: true })
+    .then(async (connection) => {
+      await connection.db("essence")
+        .collection('method')
+        .insertOne(ctx.request.body)
+        .then((result) => {
+          console.log("Created Data Method");
 
-                ctx.body = JSON.stringify(result)
-            }).catch((err) => {
-                console.error(err)
+          ctx.body = JSON.stringify(result);
+        }).catch((err) => {
+          console.error(err);
 
-            });
+        });
 
 
-        })
-        .catch((err) => console.error(err))
+    })
+    .catch((err) => console.error(err));
 });
 
 index.post("/example/:id", async (ctx) => {
-    console.log(ctx.request.body)
-    await MongoClient.connect(MONGO_URL, { useNewUrlParser: true })
-        .then(async (connection) => {
-            await connection.db("essence")
-                .collection('example')
-                .findOne({"_id": ObjectID(ctx.params.id)})
-                .then(async (result) => {
-                    ctx.request.body.edge = result.edge
-                    ctx.request.body.essence_kernel = result.essence_kernel
+  console.log(ctx.request.body);
+  await MongoClient.connect(MONGO_URL, { useNewUrlParser: true })
+    .then(async (connection) => {
+      await connection.db("essence")
+        .collection('example')
+        .findOne({ "_id": ObjectID(ctx.params.id) })
+        .then(async (result) => {
+          ctx.request.body.edge = result.edge;
+          ctx.request.body.essence_kernel = result.essence_kernel;
 
-                    await MongoClient.connect(MONGO_URL, { useNewUrlParser: true })
-                        .then(async (connection) => {
-                            await connection.db("essence")
-                                .collection('method')
-                                .insertOne(ctx.request.body)
-                                .then((res) => {
-                                console.log("Created Data Method")
+          await MongoClient.connect(MONGO_URL, { useNewUrlParser: true })
+            .then(async (connection) => {
+              await connection.db("essence")
+                .collection('method')
+                .insertOne(ctx.request.body)
+                .then((res) => {
+                  console.log("Created Data Method");
 
-                                ctx.body = JSON.stringify(res)
-                            }).catch((err) => {
-                                console.error(err)
-                            });
-                        })
-                        .catch((err) => console.error(err))
-            }).catch((err) => console.error(err));
-        })
-        .catch((err) => console.error(err))
+                  ctx.body = JSON.stringify(res);
+                }).catch((err) => {
+                  console.error(err);
+                });
+            })
+            .catch((err) => console.error(err));
+        }).catch((err) => console.error(err));
+    })
+    .catch((err) => console.error(err));
 });
 
 index.post("/rules", async (ctx) => {
-    await MongoClient.connect(MONGO_URL, { useNewUrlParser: true })
-        .then(async (connection) => {
-            await connection.db("essence")
-                .collection('rule')
-                .insertOne(ctx.request.body)
-                .then((result) => {
-                ctx.body = JSON.stringify(result)
-            }).catch((err) => {
-                console.error(err)
-            });
-        })
-        .catch((err) => console.error(err))
+  await MongoClient.connect(MONGO_URL, { useNewUrlParser: true })
+    .then(async (connection) => {
+      await connection.db("essence")
+        .collection('rule')
+        .insertOne(ctx.request.body)
+        .then((result) => {
+          ctx.body = JSON.stringify(result);
+        }).catch((err) => {
+          console.error(err);
+        });
+    })
+    .catch((err) => console.error(err));
 });
 
 index.get("/rules", async (ctx) => {
-    await MongoClient.connect(MONGO_URL, { useNewUrlParser: true })
-        .then(async (connection) => {
-            await connection.db("essence").collection('rule').find({}).toArray().then((result) => {
-                console.log("Database connection established")
-                ctx.body = JSON.stringify(result)
-            }).catch((err) => console.error(err));
-        })
-        .catch((err) => console.error(err))
+  await MongoClient.connect(MONGO_URL, { useNewUrlParser: true })
+    .then(async (connection) => {
+      await connection.db("essence").collection('rule').find({}).toArray().then((result) => {
+        console.log("Database connection established");
+        ctx.body = JSON.stringify(result);
+      }).catch((err) => console.error(err));
+    })
+    .catch((err) => console.error(err));
 });
 
 index.put("/rules/:id", async (ctx) => {
-    delete ctx.request.body._id
-    delete ctx.request.body.color
-    var newvalues = { $set: ctx.request.body }
+  delete ctx.request.body._id;
+  delete ctx.request.body.color;
+  var newvalues = { $set: ctx.request.body };
 
-    await MongoClient.connect(MONGO_URL, { useNewUrlParser: true })
-        .then(async (connection) => {
-            await connection.db("essence").collection('rule')
-                .updateOne({"_id": ObjectID(ctx.params.id)},newvalues).then((result) => {
-                console.log("Database connection established")
-                ctx.body = JSON.stringify(result)
-            }).catch((err) => console.error(err));
-        })
-        .catch((err) => console.error(err))
+  await MongoClient.connect(MONGO_URL, { useNewUrlParser: true })
+    .then(async (connection) => {
+      await connection.db("essence").collection('rule')
+        .updateOne({ "_id": ObjectID(ctx.params.id) }, newvalues).then((result) => {
+          console.log("Database connection established");
+          ctx.body = JSON.stringify(result);
+        }).catch((err) => console.error(err));
+    })
+    .catch((err) => console.error(err));
 });
 
 app.use(index.routes()).use(index.allowedMethods());
